6528b7e88dbe4ed94b94644f4db33efad34b906a
Master-Hash <hash@hash.memorial>
2025-01-19 17:23:12 +0100

    feat: 一元表达式 ast (有优化空间)

--- src/ast.rs
+++ src/ast.rs
@@ -22,5 +22,74 @@ pub struct Block {
 
 #[derive(Debug)]
 pub struct Stmt {
-    pub num: i32,
+    pub exp: Exp,
+}
+
+// Exp         ::= LOrExp;
+// PrimaryExp  ::= "(" Exp ")" | Number;
+// Number      ::= INT_CONST;
+// UnaryExp    ::= PrimaryExp | UnaryOp UnaryExp;
+// UnaryOp     ::= "+" | "-" | "!";
+// MulExp      ::= UnaryExp | MulExp ("*" | "/" | "%") UnaryExp;
+// AddExp      ::= MulExp | AddExp ("+" | "-") MulExp;
+// RelExp      ::= AddExp | RelExp ("<" | ">" | "<=" | ">=") AddExp;
+// EqExp       ::= RelExp | EqExp ("==" | "!=") RelExp;
+// LAndExp     ::= EqExp | LAndExp "&&" EqExp;
+// LOrExp      ::= LAndExp | LOrExp "||" LAndExp;
+
+#[derive(Debug)]
+pub struct Exp {
+    // pub l_or_exp: LOrExp,
+    pub unary_exp: UnaryExp,
+}
+
+#[derive(Debug)]
+pub enum PrimaryExp {
+    Exp(Box<Exp>),
+    Number(i32),
+}
+
+#[derive(Debug)]
+pub enum UnaryExp {
+    Primary(PrimaryExp),
+    PlusUnary(Box<UnaryExp>),
+    MinusUnary(Box<UnaryExp>),
+    NotUnary(Box<UnaryExp>),
+}
+
+#[derive(Debug)]
+pub enum MulExp {
+    UnaryExp(UnaryExp),
+    BinaryMulExp(Box<MulExp>, UnaryExp),
+}
+
+#[derive(Debug)]
+
+pub enum AddExp {
+    MulExp(MulExp),
+    BinaryAddExp(Box<AddExp>, MulExp),
+}
+
+#[derive(Debug)]
+pub enum RelExp {
+    AddExp(AddExp),
+    BinaryRelExp(Box<RelExp>, AddExp),
+}
+
+#[derive(Debug)]
+pub enum EqExp {
+    RelExp(RelExp),
+    BinaryEqExp(Box<EqExp>, RelExp),
+}
+
+#[derive(Debug)]
+pub enum LAndExp {
+    EqExp(EqExp),
+    BinaryLAndExp(Box<LAndExp>, EqExp),
+}
+
+#[derive(Debug)]
+pub enum LOrExp {
+    LAndExp(LAndExp),
+    BinaryLOrExp(Box<LOrExp>, LAndExp),
 }
